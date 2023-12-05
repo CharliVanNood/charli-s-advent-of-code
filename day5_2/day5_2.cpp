e2b7f20c90c8d69ba7c2e27ed324d4d45eab65fb
New-Connected <123391187+CharliVanNood@users.noreply.github.com>
2023-12-05 23:30:20 +0100

    moving to c++ for day 5 part 2

---
+++ day5_2/day5_2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	string data[256];
+
+	string seeds[22];
+	string seedToSoil[50][3];
+
+	string currentRead = "seeds";
+
+	string line;
+	ifstream file("Z:/charli-s-advent-of-code/data/day5.txt");
+
+	int line_ = 0;
+	while (getline(file, line)) {
+		data[line_] = line;
+		line_ += 1;
+	}
+	file.close();
+
+	for (int line_ = 0; line_ < sizeof(data) / sizeof(string); line_++) {
+		string line = data[line_];
+		if (line != "") {
+			if (line.find(":") < 100) {
+				line = line.replace(line.find(":"), 1, "");
+			}
+
+			string lineSplit[22];
+
+			int split__ = 0;
+			stringstream split_(line);
+			string splitChar;
+			while (split_ >> splitChar) {
+				lineSplit[split__] = splitChar;
+				split__++;
+			}
+			if (lineSplit[0] == "seeds") {
+				cout << "seeds found" << endl;
+				for (int seed = 1; seed < sizeof(lineSplit) / sizeof(string) - 1; seed++) {
+					seeds[seed - 1] = lineSplit[seed];
+				}
+			}
+			else if (
+				lineSplit[0] == "seed-to-soil" || 
+				lineSplit[0] == "soil-to-fertilizer" || 
+				lineSplit[0] == "fertilizer-to-water" || 
+				lineSplit[0] == "water-to-light" || 
+				lineSplit[0] == "light-to-temperature" || 
+				lineSplit[0] == "temperature-to-humidity" || 
+				lineSplit[0] == "humidity-to-location"
+				) {
+				currentRead = lineSplit[0];
+			}
+
+			if (currentRead == "seed-to-soil")
+		}
+	}
+}
